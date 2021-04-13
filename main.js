ea105472564b876edc860603ef1db3aa6749027a
RacMcT <raccmct@gmail.com>
2021-04-12 19:53:03 -0500

    changed comments to be ordered

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //'use strict'
 
-// JOKE OF THE DAY CODE:
-
-//create the fetch call to the api address
-//on the html page the end-user is able to generate random joke
-//user can click on the button to generate the joke
 
 
-// let fetchDrink = fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php`)
+// NEED TO REQUEST TEMP ACCESS TO CORS-ANYWHERE TO USE EACH DAY! 
+//fetch api data for cocktail- user presses button and cocktail appears underneath
+//function that fetches a random cocktail
+
+//cors-anywhere.herokuapp.com/
+
 
 function fetchDrinks(){
   fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`).then(response => response.json()) // error occuring here, but not sure how to fix or validate?
@@ -30,6 +30,11 @@ function displayCocktail(data) {
 
 }
 
+// JOKE OF THE DAY CODE:
+
+//create the fetch call to the api address
+//on the html page the end-user is able to generate random joke
+//user can click on the button to generate the joke
 
 let getButton = document.getElementById("jokeBtn");
 
@@ -60,92 +65,6 @@ getButton.addEventListener('click', function(){
   }
 
 
-  // FIX BUGS WHEN GET A CHANCE
-// COCKTAIL INPUT FORM API CODE:
-
-  //getting button element from dom and adding event listener to it
-//document.getElementById("button").addEventListener("click", function(){
-//   console.log("the button was clicked");
-
-//   //getting input search term for ingredient by user
-// // let input = document.getElementById("input");
-// let searchTerm = input.value;
-// console.log("user input = ", searchTerm);
-// fetchSearch(searchTerm);
-// })
-
-// NEED TO REQUEST TEMP ACCESS TO CORS-ANYWHERE TO USE EACH DAY! 
-//fetch api data for cocktail- user presses button and cocktail appears underneath
-//function that fetches a random cocktail
-
-//cors-anywhere.herokuapp.com/
 
 
 
-
-
-
-// // Display drinks based on ingredient 
-// function displayDrinks(x) {
-//   console.log(x)
-//   x.forEach(drink => {
-//     if(drink.strDrink === input.value) {
-//       let ul = document.getElementById("drinkListUl")
-//       let string = "strIngredient"
-//       for(let i = 1; drink[string + i] != null; i++) {
-//         let li = document.createElement("li")
-//         li.innerText = `${drink[string + i]}`
-//         ul.appendChild(li)
-//       }
-//     }
-//   })
-//   }
-
-
-
-// // function arrToUl(div, drinksList) {
-// //   console.log(drinksList);
-// //   var ul = document.getElementById('drinksListUl');
-// //   div.appendChild(ul); // append the created ul to the root
-// //             //RETURNING UNDEFINED IN DOM, BUT OKAY AS ARRAY IN CONSOLE?!?!?!?!
-// //     li = document.createElement('li'); // create a new list item
-// //     // let s = drinkList.strDrink;
-// //     // console.log(s);
-// //     li.appendChild(document.createTextNode(drinksList.strDrink)); // append the text to the li
-// //     ul.appendChild(li); // append the list item to the ul
-// //   };
-// // // }
-// //  arrToUl(div, drinksList);
-// // // })
-// // })
-// //}
-
-
-
-
-
-
-// // function render(results) {
-// //   const root = document.querySelector("#root");
-// //   const list = results.map(itemToLi).join("");
-
-// //   root.innerHTML = `<ul>
-// //     ${list}
-// //   </ul>`
-// // }
-
-// // function itemToLi(item) {
-// //   return `<li>${item.name}</li>`;
-// // }
-
-
-
-// // let processInput =function(drink) {
-// //   // newContent and extracted .json result
-// //   const newDrinks = document.createElement('li');
-// //   // getting the element from html file 
-// //   const currentDiv = document.getElementById("drinks")
-// //   // add text node cocktail to child element 
-// //   currentDiv.appendChild(newDrinks); // append the list items to the ul
-  
- 
